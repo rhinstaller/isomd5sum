d47b9f82fd1c8f7a0f8cdb1c454c6d89f8274423
Jeremy Katz <katzj@redhat.com>
2007-11-20 17:19:32 -0500

    add copying information

--- md5.c
+++ md5.c
@@ -1,6 +1,4 @@
 /*
- * $Id$
- *
  * This code implements the MD5 message-digest algorithm.
  * The algorithm is due to Ron Rivest.  This code was
  * written by Colin Plumb in 1993, no copyright is claimed.
