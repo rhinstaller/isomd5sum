dd702570c9230d8ec4b6e55372d16ee65179a771
Brian C. Lane <bcl@redhat.com>
2010-03-24 10:05:38 -0700

    Clean up mediaCheckFile and printMD5SUM result codes
    Change mediaCheckFile to return -2 when file open fails
    Add new symbolic result code for failing to find the file -
    ISOMD5SUM_FILE_NOT_FOUND.
    printMD5SUM now returns result codes instead of calling exit
    Add new function to handle rc value and print result string
    Add EXIT STATUS section to manpage

--- libcheckisomd5.h
+++ libcheckisomd5.h
@@ -5,11 +5,12 @@
 #define ISOMD5SUM_CHECK_FAILED          0
 #define ISOMD5SUM_CHECK_ABORTED         2
 #define ISOMD5SUM_CHECK_NOT_FOUND       -1
+#define ISOMD5SUM_FILE_NOT_FOUND        -2
 
 /* for non-zero return value, check is aborted */
 typedef int (*checkCallback)(void *, long long offset, long long total);
 
 int mediaCheckFile(char *iso, checkCallback cb, void *cbdata);
-void printMD5SUM(char *file);
+int printMD5SUM(char *file);
 
 #endif
