3b884fd2386f3f8773255cd1b61901281988d0ff
squimrel <squimrel@users.noreply.github.com>
2017-06-10 02:11:17 +0000

    Make library C++ compatible

--- libcheckisomd5.h
+++ libcheckisomd5.h
@@ -1,6 +1,10 @@
 #ifndef __LIBCHECKISOMD5_H__
 #define __LIBCHECKISOMD5_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 enum isomd5sum_status {
     ISOMD5SUM_FILE_NOT_FOUND = -2,
     ISOMD5SUM_CHECK_NOT_FOUND = -1,
@@ -15,5 +19,9 @@ typedef int (*checkCallback)(void *, long long offset, long long total);
 int mediaCheckFile(const char *file, checkCallback cb, void *cbdata);
 int mediaCheckFD(int isofd, checkCallback cb, void *cbdata);
 int printMD5SUM(const char *file);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
