755a38bc88d15b81ea5debe33d4e94a85374e725
squimrel <squimrel@users.noreply.github.com>
2017-06-10 02:11:17 +0000

    Make library C++ compatible

--- libimplantisomd5.h
+++ libimplantisomd5.h
@@ -1,6 +1,16 @@
 #ifndef __LIBIMPLANTISOMD5_H__
 #define __LIBIMPLANTISOMD5_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int implantISOFile(char *iso, int supported, int forceit, int quiet, char **errstr);
 int implantISOFD(int isofd, int supported, int forceit, int quiet, char **errstr);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
 
