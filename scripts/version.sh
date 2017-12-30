19e6521f7f001dd1f3c9f590de5b234e606d35e8
squimrel <squimrel@users.noreply.github.com>
2017-12-30 07:58:52 +0000

    Provide a version number in case there's no git

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-echo "$(git describe --abbrev=0 --tags 2>/dev/null || echo 0)" | \
-  cut -d "-" -f1 | tr -d "\n"
+if git rev-parse --is-inside-work-tree &>/dev/null; then
+  echo "$(git describe --abbrev=0 --tags 2>/dev/null || echo 0)" | \
+    cut -d "-" -f1 | tr -d "\n"
+else
+  echo "${1}" || echo 0
+fi
